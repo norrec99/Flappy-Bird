4fea9416b315b90d253aade7116e32591419df15
norrec99 <cagataysoyer@gmail.com>
2022-04-02 02:47:25 +0300

    initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  [SerializeField] float jumpForce = 15f;
+
+
+  Rigidbody2D rb;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ProcessJump();
+  }
+
+
+
+  void ProcessJump()
+  {
+    if (Input.GetKey(KeyCode.Space))
+    {
+      rb.velocity = Vector2.up * jumpForce;
+    }
+  }
+
+}
